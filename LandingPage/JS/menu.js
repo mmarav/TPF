10c94c47cd3c21d89c0d77ab216a99e68ade2ab9
mmarav <100808438+mmarav@users.noreply.github.com>
2022-10-22 21:24:51 -0300

    Add files via upload

---
+++ LandingPage/JS/menu.js
@@ -0,0 +1,9 @@
+const ham = document.querySelector(".ham");
+const menu = document.querySelector(".menu-navegacion");
+
+console.log(menu)
+console.log(ham)
+
+ham.addEventListener("click", () => {
+    alert("click")
+})
