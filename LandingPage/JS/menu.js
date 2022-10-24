b617ea4be0ac1835182079dc402ea02db39f8742
mmarav <100808438+mmarav@users.noreply.github.com>
2022-10-24 09:59:03 -0300

    Update menu.js

--- LandingPage/JS/menu.js
+++ LandingPage/JS/menu.js
@@ -1,12 +1,16 @@
-const ham =document.querySelector(".ham"),
-      menu=document.querySelector(".menu-navegacion");
-      
-ham.addEventListener("clic",()=>{
-    menu.classList.toggle("spread");
-} )
+const ham = document.querySelector(".ham");
+const menu = document.querySelector(".menu-navegacion");
 
-ventana.addEventListener("clic", e =>{
-    if (menu.classList.contains("spread") && e.target != menu && e.target != ham){
+console.loh(menu)
+console.log(ham)
+
+ham.addEventListener("clic", () => {
+    menu.classList.toggle("spread");
+})
+
+window.addEventListener("clic", e => {
+    if(menu.classList.contains("spread") && e.target != menu && e.target != ham) {
+        
         menu.classList.toggle("spread")
     }
 })
