9c5ea8e9cf059d03db8032d779dd61f41d96e502
mmarav <100808438+mmarav@users.noreply.github.com>
2022-12-19 03:32:14 -0300

    Update ligthbox.js

--- LandingPage/JS/ligthbox.js
+++ LandingPage/JS/ligthbox.js
@@ -1,29 +1,25 @@
-const imagenes = document.querySelectorAll(".img-galeria")
-const imagenesLight = document.querySelector(".agregar-imagen")
-const contenedorLight = document.querySelector(".imagen-light")
-const ham1 = document.querySelector(".ham");
-
-console.log(imagenes)
-console.log(imagenesLight)
-console.log(contenedorLight)
+const hamburguer1 = document.querySelector(".hamburguer");
+const imagenes = document.querySelectorAll(".img-galeria");
+const imagenesLight = document.querySelector(".agregar-imagen");
+const contenedorLight = document.querySelector(".imagen-light");
 
 imagenes.forEach(imagen => {
     imagen.addEventListener("click", () => {
-        aparecerImagen (imagen.getAttribute("src"))
+        aparecerImagen(imagen.getAttribute("src"));
     })
 })
 
-contenedorLight.addEventListener("click", () => {
+contenedorLight.addEventListener("click", (e) => {
     if (e.target !== imagenesLight) {
-        contenedorLight.classList.toggle("show")
-        imagenesLight.classList.toggle("showImage")
-        ham1.style.opacity ="1"
+        contenedorLight.classList.toggle("show");
+        imagenesLight.classList.toggle("showImage");
+        hamburguer1.style.opacity = "1";
     }
 })
 
 const aparecerImagen = (imagen) => {
     imagenesLight.src = imagen;
-    contenedorLight.classList.toggle("show")
-    imagenesLight.classList.toggle("showImage")
-    ham1.style.opacity = "0"
+    contenedorLight.classList.toggle("show");
+    imagenesLight.classList.toggle("showImage");
+    hamburguer1.style.opacity = "0";
 }
